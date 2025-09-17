5057627bedf29c8859be718f58f222e80bf6bf63
ayush <ayushkumar85890@gmail.com>
2025-09-18 01:00:26 +0530

    common operating in python

--- practice set.py
+++ practice set.py
@@ -1,38 +1,39 @@
-# print twinkle twinkle little star (''') me kiya jata hai
-print('''Twinkle, twinkle, little star,
-How I wonder what you are!
-Up above the world so high,
-Like a diamond in the sky.
+# arthmatic
+a=6
+b=6
+c=a+b
+print(c)
 
-When the blazing sun is gone,
-When he nothing shines upon,
-Then you show your little light,
-Twinkle, twinkle, all the night.
+#assignment opprater
+a=45
+a-=100
+print(a)
 
-Then the traveler in the dark
-Thanks you for your tiny spark,
-How could he see where to go,
-If you did not twinkle so?
+a=4-2
+print(a)
 
-In the dark blue sky you keep,
-Often through my curtains peep
-For you never shut your eye,
-Till the sun is in the sky.
+# comprision opprater
+a=5<1 
+print(a)
 
-As your bright and tiny spark
-Lights the traveler in the dark,
-Though I know not what you are,
-Twinkle, twinkle, little star.
-''')
+print(5==5);# equal to
 
-# 5 table
-for i in range (1,11):
-    print(f"s*{i} = {5 * i}")
+print(5!=5); # not equal to
 
-    # pip install modelus
-    import pyttsx3
-engine = pyttsx3.init()
-engine.say("hello")
-engine.runAndWait()
+#logical oprater
+a=False or True
+print(a)
+
+# truth table
+e=True and False
+print("true or false is",True or false);
+print("false or true is",False or True);
+print("false or false is",False or False);
+
+# truth table and
+
+print ("true and false is",True and False);
+print("true and true is", True and True);
+
 
 
