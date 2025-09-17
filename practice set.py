6c0182e217ff2de62d54e0ec5ea7ca150142f815
ayush <ayushkumar85890@gmail.com>
2025-09-18 00:28:11 +0530

    practice set

---
+++ practice set.py
@@ -0,0 +1,38 @@
+# print twinkle twinkle little star (''') me kiya jata hai
+print('''Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.
+
+Then the traveler in the dark
+Thanks you for your tiny spark,
+How could he see where to go,
+If you did not twinkle so?
+
+In the dark blue sky you keep,
+Often through my curtains peep
+For you never shut your eye,
+Till the sun is in the sky.
+
+As your bright and tiny spark
+Lights the traveler in the dark,
+Though I know not what you are,
+Twinkle, twinkle, little star.
+''')
+
+# 5 table
+for i in range (1,11):
+    print(f"s*{i} = {5 * i}")
+
+    # pip install modelus
+    import pyttsx3
+engine = pyttsx3.init()
+engine.say("hello")
+engine.runAndWait()
+
+
